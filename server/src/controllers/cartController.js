536aec81d940676b89fccba475e4fd3355c87c3b
Sai <jamisettisai@gmail.com>
2026-03-23 20:28:37 +0530

    spaces are decresed

--- server/src/controllers/cartController.js
+++ server/src/controllers/cartController.js
@@ -8,20 +8,14 @@ const addToCart = async (req, res) => {
         if (!userId || !productId) {
             return res.status(400).json({ message: 'userId and productId are required' });
         }
-
-        
         const product = await Product.findById(productId);
         if (!product) return res.status(404).json({ message: 'Product not found' });
-
-       
         if (!shopId) shopId = product.shopId?.toString();
         if (!shopId) {
             return res.status(400).json({ message: 'shopId could not be determined for this product' });
         }
-
         const price = product.price?.sellingprice || 0;
         let cart = await Cart.findOne({ userId });
-
         if (!cart) {
             cart = new Cart({
                 userId,
